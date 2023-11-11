be5dce75f86decb07c5960cb0d2cd91e8424f1f2
Beatriz Gomes <bea21gomes@gmail.com>
2023-11-11 17:52:42 -0300

    Raiz do projeto

---
+++ src/components/ContentMain/ContentMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ContentMain.css';
+import Search from '../Search/Search';
+import Weather from '../Weather/WeatherCard'
+
+function ContentMain() {
+  return (
+   <div>Oi
+    <Search/>
+   </div>
+  );
+}
+
+export default ContentMain;
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Search.css';
+
+function Search(){
+    return (
+        <div>Oi 2</div>
+    )
+}
+
+export default Search;
